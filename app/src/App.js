70880f9eba2936406be95d5048ae1f639e447f35
Massimiliano Giroldi <max.devjs@gmail.com>
2019-04-09 23:54:37 -0300

    desafio intermediario dia1

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Square from './components/Square/index';
 import Board from './components/Board/index';
 
-const App = () => <Board />;
+const App = () => <Board qtd="9" />;
 
 export default App;
--- app/src/components/Board/index.js
+++ app/src/components/Board/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import "./styles.css";
 
-const Board = () => (
-  <div className="board"></div>
+import Square from "../Square/index";
+
+const square = qtd => {
+  let max = qtd;
+  const squares = [];
+
+  while(max--) {
+    squares.push(<Square key={max} />);
+  }
+
+  return squares;
+}
+
+const Board = ({qtd}) => (
+  <article className="board">{square(qtd)}</article>
 );
 
 export default Board;
